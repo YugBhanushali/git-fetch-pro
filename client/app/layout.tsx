2e6cc603bf1458df9491408d24ad204142b7a5e8
YugBhanushali <yughbhanushali241103@gmail.com>
2024-08-13 18:33:51 +0530

    added analytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Source_Code_Pro, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 const sourceCodePro = Source_Code_Pro({
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={robotoMono.className}>{children}</body>
+      <body className={robotoMono.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
